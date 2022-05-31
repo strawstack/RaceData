0b6c902297b15acc013a48478aaf115e79f3798e
strawstack <rhshadowgreen@gmail.com>
2022-05-30 21:48:02 -0700

    finish times for 2022 full

--- json_to_csv.js
+++ json_to_csv.js
@@ -3,6 +3,26 @@ let assert = require('node:assert');
 
 function main() {
 
+    try {
+        const json_data = fs.readFileSync('./data/json/2022_full.json', 'utf8');
+        let json = JSON.parse(json_data);
+
+        let times = [];
+
+        for (let item of json) {
+            let finish_time = item.details.at_42_2.time_of_day; 
+            if (finish_time !== null) {
+                times.push(finish_time);
+                console.log(finish_time, item.bib);
+            }
+        }
+
+        //fs.writeFileSync('./data/finish_times/2022_full_finish.csv', times.join("\n"));
+
+      } catch (err) {
+        console.error(err);
+      }
+
 }
 
 main();
